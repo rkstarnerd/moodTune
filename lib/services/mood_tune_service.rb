b4f3eb5c381dd5e95c8c42d8cb0df6dfd9b3024a
Gemille Ford <gemille.ford@manheim.com>
2018-03-17 15:30:45 -0400

    refactor

--- lib/services/mood_tune_service.rb
+++ lib/services/mood_tune_service.rb
@@ -12,24 +12,36 @@ class MoodTuneService
 
   def get_tracks_for_mood(mood, user_id, max_num_playlists, max_num_tracks)
     playlists = @spotify_client.get_user_playlists(user_id, max_num_playlists).shuffle!
-
-    playlist_tracks = nil
-
-    while playlist_tracks.nil?
-      playlist = playlists.shift
-      playlist_id = @spotify_client.get_playlist_id(playlist.to_json)
-      playlist_tracks = @spotify_client.get_playlist_tracks(user_id, playlist_id, max_num_tracks)
-    end
-
-    get_playlist_tracks_with_mood(mood, playlist_tracks)
+    get_playlist_tracks_with_mood(mood, max_num_tracks, playlists, user_id)
   end
 
   private
 
-    def get_playlist_tracks_with_mood(mood, playlist_tracks)
-      playlist_tracks.select do |track|
-        track_mood = @gracenote_client.get_track_mood(track.to_json)
-        track if track_mood == mood
-      end
+  def get_playlist_tracks(max_num_tracks, playlists, user_id)
+    playlist_tracks = nil
+
+    while playlist_tracks.nil?
+      playlist = playlists.shuffle.shift
+      playlist_id = @spotify_client.get_playlist_id(playlist.to_json)
+      playlist_tracks = @spotify_client.get_playlist_tracks(user_id, playlist_id, max_num_tracks)
     end
+
+    playlist_tracks
+  end
+
+  def get_playlist_tracks_with_mood(mood, max_num_tracks, playlists, user_id)
+    tracks_for_mood = []
+
+    while tracks_for_mood.empty?
+      playlist_tracks = get_playlist_tracks(max_num_tracks, playlists, user_id)
+
+      tracks_for_mood =
+        playlist_tracks.select do |track|
+          track_mood = @gracenote_client.get_track_mood(track.to_json)
+          track if track_mood == mood.capitalize
+        end
+    end
+
+    tracks_for_mood
+  end
 end
--- spec/lib/services/mood_tune_service_spec.rb
+++ spec/lib/services/mood_tune_service_spec.rb
@@ -1,14 +1,16 @@
 require 'json'
 require 'rspec'
-require 'services/service'
+require 'services/mood_tune_service'
 require 'clients/gracenote_client'
 
-describe Service do
+describe MoodTuneService do
 
   it 'should match mood to tracks in a playlist' do
-    tracks = Service.new.get_tracks_for_mood("Sensual", "rkmyg1", 10, 10)
+    moods = File.read('spec/helpers/moods.txt').lines.shuffle
+    input_mood = moods.first.strip.capitalize
+    tracks = MoodTuneService.new.get_tracks_for_mood(input_mood, "1214851524", 10, 10)
     mood = GracenoteClient.new.get_track_mood(tracks.shuffle.first.to_json)
 
-    expect(mood).to eq("Sensual")
+    expect(mood).to eq(input_mood)
   end
 end
