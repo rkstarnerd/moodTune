3b6ba62ad045804f06852beecc4cf56bffe453d3
Gemille Ford <gemille.ford@manheim.com>
2018-03-17 15:29:30 -0400

    add space between setup and expectations

--- spec/lib/clients/gracenote_client_spec.rb
+++ spec/lib/clients/gracenote_client_spec.rb
@@ -6,6 +6,7 @@ describe GracenoteClient do
   it 'gets the mood of a track' do
     track = File.read('spec/helpers/spotify_track_object.json')
     client = GracenoteClient.new
+
     expect(client.get_track_mood(track)).to be_instance_of String
     expect(client.get_track_mood(track)).to eq('Sensual')
   end
