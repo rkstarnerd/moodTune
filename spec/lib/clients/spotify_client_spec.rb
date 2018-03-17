7e8448828e5efdcd6def7d36701a9d77249447d8
Gemille Ford <gemille.ford@manheim.com>
2018-03-17 15:28:57 -0400

    change spotify user_id

--- spec/lib/clients/spotify_client_spec.rb
+++ spec/lib/clients/spotify_client_spec.rb
@@ -10,7 +10,7 @@ describe SpotifyClient do
 
   it 'gets a user\'s saved playlists' do
     max_num = Random.rand(10)
-    user_playlists = @client.get_user_playlists("rkmyg1", max_num)
+    user_playlists = @client.get_user_playlists("1214851524", max_num)
 
     expect(user_playlists.count).to eq(max_num)
   end
@@ -19,13 +19,13 @@ describe SpotifyClient do
     max_num = Random.rand(10)
     @playlist = File.read('spec/helpers/spotify_playlist_object.json')
     playlist_hash = JSON.parse(@playlist)
-    playlist_tracks = @client.get_playlist_tracks("rkmyg1", playlist_hash["id"], max_num)
+    playlist_tracks = @client.get_playlist_tracks("1214851524", playlist_hash["id"], max_num)
 
     expect(playlist_tracks.count).to eq(max_num)
   end
 
   it 'gets a playlist\'s id' do
     playlist = File.read('spec/helpers/spotify_playlist_object.json')
-    expect(@client.get_playlist_id(playlist)).to eq("5xhBuGO5WOYF0UnI0YYlEk")
+    expect(@client.get_playlist_id(playlist)).to eq("1d5gw19rQpEgNRlSWBhMGt")
   end
 end
